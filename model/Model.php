905cc090e171e8bf52f271c86e4d5b054b227c0f
Cyberproton <cyberprotonAtMailbox@gmail.com>
2021-11-24 08:37:18 +0700

    update model methods

--- model/Model.php
+++ model/Model.php
@@ -30,7 +30,28 @@ class Model
         return $this->get_validation_message(null, true);
     }
 
+    public function validate_data(array $data): array
+    {
+        return $this->get_validation_message(null, true);
+    }
+
+    public function validate_and_load(array $data): array 
+    {
+        $res = $this->validate_data($data);
+        if (!$res["is_successful"])
+        {
+            return $res;
+        }
+        $this->load($data);
+        return $this->get_validation_message("Success", true);
+    }
+
     public function get_validation_message(?string $message, bool $is_successful): array 
+    {
+        return [ "message" => $message, "is_successful" => $is_successful];
+    }
+
+    public static function get_validation_message_static(?string $message, bool $is_successful): array 
     {
         return [ "message" => $message, "is_successful" => $is_successful];
     }
